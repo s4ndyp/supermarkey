7355aec7baa4ee03b71a44be3526c13600123a5f
s4ndyp <96690821+s4ndyp@users.noreply.github.com>
2026-01-26 19:43:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
- * SuperLijst Service Worker - Versie 22 (Agressieve Caching)
+ * SuperLijst Service Worker - Versie 23 (Agressieve Caching)
  * Geoptimaliseerd voor snelle activatie en betrouwbare offline toegang.
  */
 
-const CACHE_NAME = 'superlijst-assets-v22';
+const CACHE_NAME = 'superlijst-assets-v23';
 
 // De bestanden die nodig zijn voor de "App Shell"
 const urlsToCache = [
